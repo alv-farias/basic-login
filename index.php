3c8270ac6469a11b3b41eeeb62024b2599a2bfd7
Álvaro Farias <alvardev20@gmail.com>
2024-11-27 09:27:41 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Entendendo Formulários</title>
+</head>
+<body>
+<div class="page">
+        <form method="POST" action="newpage.php" class="formLogin">
+            <h1>Login</h1>
+            <p>Digite os seus dados de acesso no campo abaixo.</p>
+            <label for="email">E-mail</label>
+            <input name="email" type="email" placeholder="Digite seu e-mail" autofocus="true" />
+            <label for="password">Senha</label>
+            <input name="password" type="password" placeholder="Digite seu Senha" />
+            <a href="/">Esqueci minha senha</a>
+            <input type="submit" value="Acessar" class="btn" />
+        </form>
+    </div>
+</body>
+</html>
---
+++ newpage.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Page</title>
+</head>
+<body>
+    <h1>Olá <?php echo htmlspecialchars($_POST['email'])?></h1>
+</body>
+</html>
